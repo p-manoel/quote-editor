49cccb34f1a8ad6def2d8bf39339c771cf0c3f6f
Pedro Manoel <pedro.marques@nomadglobal.com>
2024-02-09 23:02:14 -0300

    Create Company model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_22_003129) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_10_020006) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "quotes", force: :cascade do |t|
     t.string "name", null: false
